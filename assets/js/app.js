c465c9bf16ae1b53e24dee1442316917713f7252
imranShaik065 <imran.ahmed@apisec.ai>
2023-03-07 12:12:57 +0530

    run a sample api updated netbanking pre-run api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
 
     scan();
-    hidecheck();
+    runAsampleAPI();
     $("#apiurl").addClass("d-none");
-    $("#bearertoken").addClass("d-none");
-    $("#emailpro2").addClass("d-none");
-    $("#licensekey2").addClass("d-none");
+
 
     $("[rel=tooltip]").tooltip({ placement: 'right' });
 
@@ -111,19 +109,19 @@ function scan() {
                             if (testresult.data == 'Security Test Execution') {
                                 $('#settingdark').css('filter', 'none')
                                 $('#generate').removeClass('d-none')
-                                $('#generate').css('color', '#6c757d')
+                                $('#generate').css('color', '#025c7a', 'font-weight', '600')
                                 $('.hr-line1').css('border-bottom', '3px solid #025c7a')
                             }
                             if (testresult.data == 'Preparing Test Results') {
                                 $('#targetNew').css('filter', 'none')
                                 $('#running').removeClass('d-none')
-                                $('#running').css('color', '#6c757d')
+                                $('#running').css('color', '#025c7a', 'font-weight', '600')
                                 $('.hr-line2').css('border-bottom', '3px solid #025c7a')
                             }
                             if (testresult.data == 'Scan completed') {
                                 $('#reporticon').css('filter', 'none')
                                 $('#preparing').removeClass('d-none')
-                                $('#preparing').css('color', '#6c757d');
+                                $('#preparing').css('color', '#025c7a', 'font-weight', '600');
                                 $('#openAPISpec').val('');
                                 $('#btn').prop('disabled', false);
                                 $('#loadingresultfree').addClass('d-none')
@@ -154,7 +152,6 @@ function scan() {
                             "Content-Type": "application/json"
                         },
                         success: function (resultData) {
-                            console.log('resultData', resultData);
                             var viewResult = resultData;
                             var apispecification = viewResult.data.openAPISpec;
                             var name = viewResult.data.name;
@@ -311,7 +308,6 @@ function scan() {
                                 var xValues = ["Vulnerability", "Value Data", "Configuration", "Authentication"];
                                 var yValues = [vulnerabilityScore, valueDataScore, configurationScore, authenticationScore];
 
-
                                 var barColors = ["#dec15a", "#d65745", "#72ba2c", "#d65745"];
                                 mychart = new Chart("myChart", {
 
@@ -323,8 +319,10 @@ function scan() {
                                         datasets: [{
                                             backgroundColor: barColors,
                                             data: yValues,
+
                                         }],
                                     },
+
                                     options: {
 
                                         legend: { display: false },
@@ -338,12 +336,9 @@ function scan() {
                                     }
                                 })
 
+
+
                             }
-
-
-
-
-
                         },
                         error: function (xhr, status, error) {
                             // Handle any API errors here
@@ -351,11 +346,6 @@ function scan() {
 
                     })
                 }
-
-
-
-
-
 
             },
             error: function (error) {
@@ -388,9 +378,7 @@ function scan() {
 
 
 }
-function hidecheck() {
 
-}
 function updateProgress(progress) {
     var progressBar = document.querySelector('.progress-bar');
     progressBar.style.width = progress + '%';
@@ -415,15 +403,216 @@ function simulateProgress() {
 
 
 
-
-$(document).ready(function () {
+function runAsampleAPI() {
     $('#runSample').click(function (e) {
         e.preventDefault();
-        $('#openAPISpec').val('https://raw.githubusercontent.com/apisec-inc/Netbanking-Specs/main/ethicalcheck-netbanking-spec.json');
-        setTimeout(function () {
-            $("#btn").click();
-        }, 1);
-        // console.log($(''))
-        console.log('clicked');
+        $("#errorresult").addClass("d-none");
+        $.ajax({
+            url: 'https://apiseccheck-image-4w7ghmnvva-uw.a.run.app/api/v1/apiseccheck/results?project-name=Online%20Banking%20REST%20API%20jsPi',
+            method: 'GET',
+            dataType: 'json',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            success: function (resultData) {
+                console.log('resultData', resultData);
+                var viewResult = resultData;
+                var apispecification = viewResult.data.openAPISpec;
+                var name = viewResult.data.name;
+                var APIdescription = viewResult.data.description;
+                var score = viewResult.data.testSummary.overallScore;
+                var totalEndpoints = viewResult.data.testSummary.totalEndpoints;
+                // var dateTested = viewResult.data.dateTested
+                var testEnvironment = viewResult.data.testSummary.testEnvironment;
+                var vulnerabilityScore = viewResult.data.testSummary.vulnerabilityScore;
+                var valueDataScore = viewResult.data.testSummary.valueDataScore;
+                var configurationScore = viewResult.data.testSummary.configurationScore;
+                var authenticationScore = viewResult.data.testSummary.authenticationScore;
+                var dateString = viewResult.data.dateTested;
+                var date = new Date(dateString);
+                var formattedDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+                console.log(formattedDate);
+
+                var injectionsForEndpoints = viewResult.data.testSummary.injectionsForEndpoints;
+                var sixXFuzz = viewResult.data.testSummary.sixXFuzz;
+                var reflectedGetInjection = viewResult.data.testSummary.reflectedGetInjection;
+                var reflectedPOSTInjection = viewResult.data.testSummary.reflectedPOSTInjection;
+                var pii = viewResult.data.testSummary.pii;
+                // var moneyRelated = viewResult.data.testSummary.moneyRelated;
+                // var richContentUploads = viewResult.data.testSummary.richContentUploads;
+                var sslCertificateIssues = viewResult.data.testSummary.sslCertificateIssues;
+                var missingTLSHSTSHeaders = viewResult.data.testSummary.missingTLSHSTSHeaders;
+                var serverPropertiesLeakInHeaders = viewResult.data.testSummary.serverPropertiesLeakInHeaders;
+                var httpOptions = viewResult.data.testSummary.httpOptions;
+                var corsConfig = viewResult.data.testSummary.corsConfig;
+                var incrementalIDsForEndpoint = viewResult.data.testSummary.incrementalIDsForEndpoint;
+                var noAuth = viewResult.data.testSummary.noAuth;
+                var brokenAuthentication = viewResult.data.testSummary.brokenAuthentication;
+                var basicAuthentication = viewResult.data.testSummary.basicAuthentication;
+                barchart();
+                if (APIdescription === 'null') {
+                    $("#descriptionForAPI").text('No Description');
+                }
+                else {
+                    $("#descriptionForAPI").text(APIdescription)
+                }
+                if (injectionsForEndpoints === 'Passed') {
+                    $("#injection").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else if (injectionsForEndpoints === 'Failed') {
+                    $("#injection").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (sixXFuzz === 'Passed') {
+                    $("#6fuzz").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+
+                else {
+                    $("#6fuzz").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (reflectedGetInjection === 'Passed') {
+                    $("#reflectedget").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#reflectedget").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (reflectedPOSTInjection === 'Passed') {
+                    $("#reflectedpost").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#reflectedpost").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (pii === 'Passed') {
+                    $("#piiData").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#piiData").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (sslCertificateIssues === 'Passed') {
+                    $("#ssl").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#ssl").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (missingTLSHSTSHeaders === 'Passed') {
+                    $("#missing").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#missing").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (serverPropertiesLeakInHeaders === 'Passed') {
+                    $("#serverprop").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#serverprop").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (httpOptions === 'Passed') {
+                    $("#httpoption").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#httpoption").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (corsConfig === 'Passed') {
+                    $("#cors").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#cors").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (incrementalIDsForEndpoint === 'Passed') {
+                    $("#incremental").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#incremental").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (noAuth === 'Passed') {
+                    $("#noauth").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#noauth").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (brokenAuthentication === 'Passed') {
+                    $("#brokenauth").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#brokenauth").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                if (basicAuthentication === 'Passed') {
+                    $("#basicauth").html('<i class="fa fa-check-circle check" aria-hidden="true"></i>');
+                }
+                else {
+                    $("#basicauth").html('<i class="fa fa-times-circle cross" aria-hidden="true"></i>');
+                }
+
+                $('#indexpageUI').hide().html();
+                $('#indexpageUI').hide().html('#resultPageOnUI');
+                $('#indexpageUI').addClass('d-none');
+                $('#resultPageOnUI').removeClass('d-none')
+                $('#dateTested').text(formattedDate);
+                $("#apispecification").text(apispecification)
+                $("#name").text(name)
+
+                $("#totalEndpoints").text(totalEndpoints)
+                $("#testEnvironment").text(testEnvironment)
+                $("#overallScore").text(score);
+
+                function barchart() {
+                    var mychart
+                    var xValues = ["Vulnerability", "Value Data", "Configuration", "Authentication"];
+                    var yValues = [vulnerabilityScore, valueDataScore, configurationScore, authenticationScore];
+                    var barColors = ["#dec15a", "#d65745", "#72ba2c", "#d65745"];
+
+                    mychart = new Chart("myChart", {
+
+                        type: "bar",
+
+                        data: {
+                            labels: xValues,
+                            fontColor: ['#dec15a'],
+                            datasets: [{
+                                backgroundColor: barColors,
+                                data: yValues,
+                            }],
+                        },
+
+                        options: {
+
+                            legend: { display: false },
+                            title: {
+                                display: true,
+                            },
+
+                            scales: {
+                                yAxes: [{ ticks: { min: 0, stepSize: 10, max: 100 } }]
+                            }
+                        }
+                    })
+
+
+
+                }
+
+
+
+
+
+            },
+            error: function (xhr, status, error) {
+                // Handle any API errors here
+            }
+
+        })
+
     })
-});
+}
