a0fbe4c238c35e18f8cf2e1953a5878aae1375e3
imranShaik065 <imran.ahmed@apisec.ai>
2023-10-06 15:06:55 +0530

    specAnalysis not showing in stg environment issue fixed

--- assets/js/environment.js
+++ assets/js/environment.js
@@ -1,10 +1,10 @@
 
 
 export function getServer() {
-
-    //test server
-    // return "http://5.161.99.171:8080"
-
-    //prod server
-    return "https://apiseccheck-image-4w7ghmnvva-uw.a.run.app"
+  //test server
+  // return "http://5.161.99.171:8080";
+  //stg server
+  // return "http://apisecfree-stg-ui.apisec.ai:8080";
+  //prod server
+  return "https://apiseccheck-image-4w7ghmnvva-uw.a.run.app";
 }
